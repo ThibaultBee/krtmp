7934bb2275bea5d56aed858ecf01d40ee28497dd
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2025-05-14 14:48:00 +0200

    fu flvparser-cli

---
+++ samples/flvparser-cli/src/main/kotlin/Main.kt
@@ -0,0 +1,92 @@
+package io.github.thibaultbee.krtmp.flvparser.cli
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.Context
+import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import io.github.thibaultbee.krtmp.flv.FLVDemuxer
+import io.github.thibaultbee.krtmp.flv.tags.FLVData
+import io.github.thibaultbee.krtmp.flv.tags.FLVTag
+import io.github.thibaultbee.krtmp.flv.tags.audio.ExtendedAudioData
+import io.github.thibaultbee.krtmp.flv.tags.audio.LegacyAudioData
+import io.github.thibaultbee.krtmp.flv.tags.script.ScriptDataObject
+import io.github.thibaultbee.krtmp.flv.tags.video.ExtendedVideoData
+import io.github.thibaultbee.krtmp.flv.tags.video.LegacyVideoData
+import kotlinx.io.files.Path
+
+class FLVParser : CliktCommand() {
+    override fun help(context: Context): String {
+        return "Parse a FLV file"
+    }
+
+    val filePath: String by option("-i", "--input", help = "The FLV file to parse")
+        .required()
+
+    private fun prettyTag(index: Int, tag: FLVTag): String {
+        return """
+        |FLV Tag: [$index]
+        |  Type: ${tag.type}
+        |  Timestamp: ${tag.timestampMs} ms
+        |  Data: [${prettyData(tag.data)}]
+    """.trimMargin()
+    }
+
+    private fun prettyData(data: FLVData): String {
+        return when (data) {
+            is LegacyAudioData -> {
+                """
+            |Legacy Audio Data: Sound Format: ${data.soundFormat} Sound Rate: ${data.soundRate} Sound Size: ${data.soundSize} Sound Type: ${data.soundType} Body: ${data.body}
+            """.trimMargin()
+            }
+
+            is ExtendedAudioData -> {
+                """
+            |Extended Audio Data: Packet Type: ${data.packetType} Packet Descriptor: ${data.packetDescriptor} ModExs: ${data.modExs} Body: ${data.body}
+            """.trimMargin()
+            }
+
+            is LegacyVideoData -> {
+                """
+            |Legacy Video Data: Codec ID: ${data.codecID} Frame Type: ${data.frameType} AVCPacketType: ${data.packetType} Composition Time: ${data.compositionTime} Body: ${data.body}
+            """.trimMargin()
+            }
+
+            is ExtendedVideoData -> {
+                """
+            |Extended Video Data: Packet Type: ${data.packetType} Frame Type: ${data.frameType} Packet Descriptor: ${data.packetDescriptor} ModExs: ${data.modExs} Body: ${data.body}
+            """.trimMargin()
+            }
+
+            is ScriptDataObject -> {
+                """
+            |Script Data Object: Name: ${data.name} Value: ${data.value}
+            """.trimMargin()
+            }
+
+            else -> data.toString()
+        }
+    }
+
+    override fun run() {
+        require(filePath.endsWith(".flv")) { "The file must be a .flv file" }
+
+        echo("Parsing FLV file: $filePath")
+        val path = Path(filePath)
+        val parser = FLVDemuxer(path = path)
+        val header = parser.decodeFlvHeader()
+        echo("Parsed FLV header: $header")
+        var i = 0
+        while (!parser.isEmpty) {
+            val tag = parser.decodeRaw()
+            try {
+                val decodedTag = tag.decode()
+                echo(prettyTag(i++, decodedTag))
+            } catch (t: Throwable) {
+                echo("${i++}: failed to decode: ${t.message}")
+            }
+        }
+    }
+}
+
+fun main(args: Array<String>) = FLVParser().main(args)
